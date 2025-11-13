94b67693233a62b559413b60b217c9faddd8d933
klsdevops <90503660+klsdevops@users.noreply.github.com>
2025-11-12 22:17:04 -0700

    springboot java

---
+++ springboot-docker-lab/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// This is a minimal Spring Boot application
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloDocker() {
+		return "Hello from the Dockerized Spring Boot Application!";
+	}
+}
